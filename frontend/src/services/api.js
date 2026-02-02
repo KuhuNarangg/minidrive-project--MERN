674583dd39854fc913272fa9fdc7b59abb5eb0a8
Kuhu Narang <kuhunarang@Kuhus-MacBook-Air.local>
2026-02-03 02:00:53 +0530

    Update API base URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4001/api",
+  baseURL: "https://minidrive-project-mern.onrender.com",
 });
 
 export default API;
